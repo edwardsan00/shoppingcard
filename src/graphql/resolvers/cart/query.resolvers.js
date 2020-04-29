9c5c80efd43b795e8af01df5b0a35f82c9df7479
@edwardsan00 <edwardsan00@gmail.com>
2020-04-28 22:47:23 -0500

    [ ADD ] - queryCartByCustomer

--- src/graphql/resolvers/cart/query.resolvers.js
+++ src/graphql/resolvers/cart/query.resolvers.js
@@ -3,6 +3,9 @@ import { Carts } from '../../../datalayer/actuators'
 const Query = {
   getCartById: (_, { id }) => {
     return Carts.getById({ id })
+  },
+  getCartByCustomerId: (_, { id }) => {
+    return Carts.getByCustomer({ customerId: id})
   }
 }
 
